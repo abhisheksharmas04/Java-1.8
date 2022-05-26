8d1d9275883071afdde80ee616d1b33acee73a3c
abhisheksharmas04 <abhisheksharmaas04@gmail.com>
2022-05-26 20:39:08 +0530

    Testing Git in Visual Code

--- src/com/example/hello.java
+++ src/com/example/hello.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java 1.8");
     }
 }
